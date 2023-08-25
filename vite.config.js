d5747bd4d34efb41ac9458ee351cc2a0532f8ecb
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-08-25 14:08:03 +0900

    chore(*): libモードでのビルド設定

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { resolve } from "path"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  build: {
+    lib: {
+      // 複数のエントリーポイントのディクショナリや配列にもできます
+      entry: resolve(__dirname, "lib/index.ts"),
+      name: "sketchgl",
+      // 適切な拡張子が追加されます
+      fileName: "sketchgl"
+    },
+    rollupOptions: {
+      // ライブラリにバンドルされるべきではない依存関係を
+      // 外部化するようにします
+      external: [],
+      output: {
+        // 外部化された依存関係のために UMD のビルドで使用する
+        // グローバル変数を提供します
+        globals: {}
+      }
+    }
+  }
+})

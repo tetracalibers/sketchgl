32a1bc24064d773600c966701b71a39226f4d608
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-08-25 13:54:59 +0900

    yarn format

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css"
+import typescriptLogo from "./typescript.svg"
+import viteLogo from "/vite.svg"
+import { setupCounter } from "./counter.ts"
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
@@ -21,4 +21,4 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!)

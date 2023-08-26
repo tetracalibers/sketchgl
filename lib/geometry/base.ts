197131768d49a42daedfadabe5645da116abdfc5
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-08-26 13:29:40 +0900

    refactor(ShapeGeometry): 各形状Geometryのクラスの基底クラスを実装

---
+++ lib/geometry/base.ts
@@ -0,0 +1,50 @@
+import { Geometry, DrawArg } from "../webgl/geometry"
+
+export interface AttribLocations {
+  vertices: number
+  normals?: number
+}
+
+type stringlike = string | number
+
+export interface Model {
+  vertices: stringlike[]
+  normals: stringlike[]
+  indices: stringlike[]
+}
+
+export const getRegistModelBufferFn = (model: Model) => (geometry: Geometry, locations: AttribLocations) => {
+  geometry.registAttrib({
+    location: locations.vertices,
+    components: 3,
+    buffer: new Float32Array(model.vertices.map(Number))
+  })
+
+  if (locations.normals) {
+    geometry.registAttrib({
+      location: locations.normals,
+      components: 3,
+      buffer: new Float32Array(model.normals.map(Number))
+    })
+  }
+
+  geometry.registIndices(new Uint16Array(model.indices.map(Number)))
+}
+
+export abstract class ShapeGeometry {
+  protected _geometry: Geometry
+
+  constructor(gl: WebGL2RenderingContext) {
+    this._geometry = new Geometry(gl)
+  }
+
+  abstract create(locations: AttribLocations): void
+
+  bind() {
+    this._geometry.bind()
+  }
+
+  draw(args: DrawArg) {
+    this._geometry.draw(args)
+  }
+}

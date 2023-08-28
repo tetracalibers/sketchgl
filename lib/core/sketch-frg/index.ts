59631fb9d72a6bafc41affcb365e49a2ddb1bec3
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-08-28 17:54:28 +0900

    feat(SketchFrg): フラグメントシェーダでの実験を容易にするクラスを公開

---
+++ lib/core/sketch-frg/index.ts
@@ -0,0 +1,48 @@
+import { Context } from "../context"
+import { Sketch, SketchCanvas, SketchConfig, SketchGl } from "../sketch-gl"
+import { Program } from "../../program"
+
+// @ts-ignore
+import vert from "./full-canvas.vert?raw"
+
+export interface FragmentSketchConfig extends SketchConfig {
+  frag: string
+}
+
+export interface FragmentSketchCanvas extends SketchCanvas {
+  program: WebGLProgram
+  renderToCanvas: () => void
+}
+
+export interface FragmentSketch extends Sketch {}
+
+export type FragmentSketchFn = (skCanvas: FragmentSketchCanvas) => Sketch
+
+export class SketchFrg extends SketchGl {
+  protected constructor(skCanvas: FragmentSketchConfig, sketchFn: FragmentSketchFn) {
+    const { canvas: _canvas, gl: glOptions, frag } = skCanvas
+    const { el, ...canvasOptions } = _canvas
+
+    const context = new Context(el, {
+      canvas: canvasOptions,
+      gl: glOptions
+    })
+    const { canvas, gl } = context
+
+    const _program = new Program(gl)
+    _program.attach(vert, frag)
+
+    const program = _program.get()
+    if (!program) throw new Error("Program is not available")
+
+    const renderToCanvas = () => gl.drawArrays(gl.TRIANGLE_FAN, 0, 3)
+
+    const sketch = sketchFn({ canvas, gl, program, renderToCanvas })
+
+    super(context, sketch, canvasOptions)
+  }
+
+  static init(skCanvas: FragmentSketchConfig, sketchFn: FragmentSketchFn) {
+    new SketchFrg(skCanvas, sketchFn)
+  }
+}
--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,5 @@
 export { SketchGl } from "./core/sketch-gl"
 export type { SketchFn, SketchConfig } from "./core/sketch-gl"
+
+export { SketchFrg } from "./core/sketch-frg"
+export type { FragmentSketchConfig, FragmentSketchFn } from "./core/sketch-frg"

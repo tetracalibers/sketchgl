1263ad911c84ef6b243d9d51e6fee61e5adfbbdb
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-08-30 05:36:19 +0900

    feat(SketchFilter): 画像加工用のSketchクラスを公開

--- lib/index.ts
+++ lib/index.ts
@@ -3,3 +3,6 @@ export type { SketchFn, SketchConfig } from "./core/sketch-gl"
 
 export { SketchFrg } from "./core/sketch-frg"
 export type { FragmentSketchConfig, FragmentSketchFn } from "./core/sketch-frg"
+
+export { SketchFilter } from "./core/sketch-filter"
+export type { FilterSketchConfig, FilterSketchFn } from "./core/sketch-filter"

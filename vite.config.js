e56c4d8cd73080406b2e529971633796a181e92a
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-09-05 08:51:55 +0900

    chore(*): dist/glslではなくglslをそのまま公開

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { resolve } from "path"
 import { defineConfig } from "vite"
-import { viteStaticCopy } from "vite-plugin-static-copy"
 
 export default defineConfig({
   build: {
@@ -30,15 +29,5 @@ export default defineConfig({
       }
     },
     sourcemap: true
-  },
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: "glsl/*.glsl",
-          dest: "glsl"
-        }
-      ]
-    })
-  ]
+  }
 })

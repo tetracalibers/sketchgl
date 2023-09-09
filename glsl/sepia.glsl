61bdad309dd9a98cb2b911585c8eee2140c18b5f
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-09-09 12:33:41 +0900

    feat(glsl/sepia): セピア加工ユーティリティを公開

---
+++ glsl/sepia.glsl
@@ -0,0 +1,12 @@
+#pragma glslify: toGrayscale = require("./grayscale.glsl")
+
+// セピアは、RGBで表すと(107, 74, 43)
+// 全てのフラグメントをグレイスケール化した後で、上記のRGBの比率に各フラグメントの値を調整
+const float sepiaR = 1.07;
+const float sepiaG = 0.74;
+const float sepiaB = 0.43;
+const vec3 sepiaScale = vec3(sepiaR, sepiaG, sepiaB);
+
+vec3 toSepia(vec3 color) {
+  return toGrayscale(color) * sepiaScale;
+}

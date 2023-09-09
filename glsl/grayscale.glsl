45b4c1e56b729d3bb8108a8cb4f61622a5297634
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-09-10 06:29:22 +0900

    refactor(glsl/grayscale): 定数名などを修正

--- glsl/grayscale.glsl
+++ glsl/grayscale.glsl
@@ -1,11 +1,10 @@
 // NTSC系加重平均法と呼ばれるグレイスケール変換に使われる手法に則った係数
-const float monoR = 0.298912;
-const float monoG = 0.586611;
-const float monoB = 0.114478;
-const vec3 monoScale = vec3(monoR, monoG, monoB);
+const float R_LUMINANCE = 0.298912;
+const float G_LUMINANCE = 0.586611;
+const float B_LUMINANCE = 0.114478;
 
 vec3 toGrayscale(vec3 color) {
-  return vec3(dot(color, monoScale));
+  return vec3(dot(color, vec3(R_LUMINANCE, G_LUMINANCE, B_LUMINANCE)));
 }
 
 #pragma glslify: export(toGrayscale)

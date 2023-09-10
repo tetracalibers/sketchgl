5c87f8e71756310fda56742450c5ecf55cd387ef
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-09-10 13:10:33 +0900

    fix(OffscreenRenderer): 公開し忘れを修正

--- lib/renderer.ts
+++ lib/renderer.ts
@@ -1,4 +1,5 @@
 export { MRTRenderer } from "./renderer/mrt-renderer"
+export { OffscreenRenderer } from "./renderer/offscreen"
 
 export { SwapTFRenderer } from "./renderer/tf/swap-tf"
 export { SwapSpriteTFRenderer } from "./renderer/tf/swap-tf-sprite"

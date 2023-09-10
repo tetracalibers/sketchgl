f5750c1915abcc3fb084153efe60f1bdbb6b5596
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-09-10 12:04:41 +0900

    feat(glsl/voronoi2): 第一近傍距離による胞体ノイズ

---
+++ glsl/voronoi2.glsl
@@ -0,0 +1,36 @@
+#pragma glslify: hash22 = require("./hash22.glsl")
+
+// 第一近傍距離による胞体ノイズ
+vec2 voronoi2(vec2 p) {
+  // 最も近い格子点
+  vec2 i = floor(p);
+  // タイル内のどのあたりにいるか
+  vec2 f = fract(p);
+
+  // 最も近いものまでの距離
+  float distMin = 1.0;
+  // 最も近い格子点
+  vec2 iMin;
+
+  for (int y = -1; y <= 1; y++) {
+    for (int x = -1; x <= 1; x++) {
+      // (-1, -1) or (-1, 0) or (-1, 1) or
+      // ( 0, -1) or ( 0, 0) or ( 0, 1) or
+      // ( 1, -1) or ( 1, 0) or ( 1, 1)
+      vec2 offset = vec2(float(x), float(y));
+      // 隣接するタイル内のランダムな点
+      vec2 neighbor = hash22(i + offset);
+      // 隣接するタイル内のランダムな点までの距離
+      float dist = distance(neighbor + offset, f);
+      // distMinより近ければ更新
+      if (distMin > dist) {
+        iMin = neighbor;
+        distMin = dist;
+      }
+    }
+  }
+
+  return iMin;
+}
+
+#pragma glslify: export(voronoi2)

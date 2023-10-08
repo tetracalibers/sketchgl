40bfe1a08a7599ac253b27537dab63ecc2c1ce89
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-10-09 08:25:12 +0900

    feat(camera): OrbitCameraとTrackCameraを公開

--- lib/camera.ts
+++ lib/camera.ts
@@ -1,2 +1,2 @@
-export { AngleCamera } from "./camera/angle-camera"
-export { AngleCameraController } from "./camera/angle-camera-controller"
+export { OrbitCamera } from "./camera/angle-based/orbit-camera"
+export { TrackCamera } from "./camera/angle-based/track-camera"

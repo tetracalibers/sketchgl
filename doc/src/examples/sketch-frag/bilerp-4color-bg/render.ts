b3e7c961e2a0df83156e25eb309f5aacf222e9a9
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-10-09 07:33:15 +0900

    doc(*): add example

---
+++ doc/src/examples/sketch-frag/bilerp-4color-bg/render.ts
@@ -0,0 +1,39 @@
+import { SketchFrag, type SketchFragConfig, type SketchFragFn } from "sketchgl"
+import { Uniforms } from "sketchgl/program"
+
+import frag from "./index.frag?raw"
+
+const sketch: SketchFragFn = ({ gl, canvas, program, renderToCanvas }) => {
+  const uniforms = new Uniforms(gl, ["uResolution"])
+  uniforms.init(program)
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0)
+  gl.clearDepth(1.0)
+
+  return {
+    drawOnFrame() {
+      gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+      uniforms.fvector2("uResolution", [canvas.width, canvas.height])
+
+      renderToCanvas()
+    }
+  }
+}
+
+const config: SketchFragConfig = {
+  frag,
+  canvas: {
+    el: "gl-canvas",
+    fit: "screen",
+    autoResize: true
+  }
+}
+
+const sketcher = new SketchFrag(sketch)
+
+window.onload = () => {
+  sketcher.bindCanvas(config)
+  sketcher.start()
+}

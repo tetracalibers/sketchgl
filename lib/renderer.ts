fe33eb911541781172a68fc13a16636c2ca10810
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-10-11 10:06:23 +0900

    feat(UseOffLayer): OffscreenRendererを改名・改良

--- lib/renderer.ts
+++ lib/renderer.ts
@@ -1,5 +1,5 @@
+export { UseOffLayer } from "./renderer/off-layer"
 export { UseMRT } from "./renderer/mrt"
-export { OffscreenRenderer } from "./renderer/offscreen"
 
 export { SwapTFRenderer } from "./renderer/tf/swap-tf"
 export { SwapSpriteTFRenderer } from "./renderer/tf/swap-tf-sprite"
